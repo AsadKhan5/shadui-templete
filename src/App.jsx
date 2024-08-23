a3e5cd4a1c564eead1c0cdfa559e7dd99f07de30
AsadKhan5 <asadkhan58057@gmail.com>
2024-08-23 15:23:21 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div className="p-96">
+        <Button>Hello Asad</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
